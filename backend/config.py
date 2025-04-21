a54c239fa0c3549369e9ff9ad053155233504314
jsir-g <jagarcia2929@gmail.com>
2025-04-21 16:38:34 -0500

    updated file for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,18 +1,10 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env
 load_dotenv()
 
 class Config:
-    # Flask settings
     DEBUG = True
-
-    # Database
-    DB_URL = os.getenv("postgresql://neondb_owner:npg_69htXuOzSIPF@ep-gentle-wind-a5aame1u-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
-
-    # API keys
+    DB_URL = os.getenv("NEON_DB_URL")
     SPORTRADAR_API_KEY = os.getenv("SPORTRADAR_API_KEY")
-
-    # Optional future configs (JWT secret, etc.)
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
