0117eb550f0e7b14f25e7cd8418e49d2b3b82adf
jsir-g <jagarcia2929@gmail.com>
2025-04-21 16:51:42 -0500

    updated file for deployment

--- backend/app.py
+++ backend/app.py
@@ -18,4 +18,7 @@ def home():
     return "Starter Fantasy API is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
